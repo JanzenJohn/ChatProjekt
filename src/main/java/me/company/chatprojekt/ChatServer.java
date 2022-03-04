d5a251d75e7243332e1675729a4244f5efe2899a
JanzenJohn <work@johnjanzen.me>
2022-03-04 17:49:36 +0100

    Server login / logout

--- src/main/java/me/company/chatprojekt/ChatServer.java
+++ src/main/java/me/company/chatprojekt/ChatServer.java
@@ -10,14 +10,14 @@ import java.util.HashMap;
  *
  * @author john
  */
-public class ChatServer extends Server{
-    
-    public static void main(String[] args){
+public class ChatServer extends Server {
+
+    public static void main(String[] args) {
         ChatServer x = new ChatServer();
     }
     private HashMap<String, ConnectionState> connectionStates;
-    
-    public ChatServer(){
+
+    public ChatServer() {
         super(4002);
         this.connectionStates = new HashMap<>();
     }
@@ -25,13 +25,42 @@ public class ChatServer extends Server{
     @Override
     public void processNewConnection(String pClientIP, int pClientPort) {
         this.connectionStates.put(pClientIP + pClientPort, new ConnectionState());
-        this.send(pClientIP, pClientPort, "+OK Server ready");
+        this.send(pClientIP, pClientPort, "+OK Server ready! User required");
     }
 
     @Override
     public void processMessage(String pClientIP, int pClientPort, String pMessage) {
-        if (pMessage.equals("EXIT"))this.closeConnection(pClientIP, pClientPort);
-        System.out.println(pClientIP + ":" +  pClientPort);
+
+        ConnectionState state = this.connectionStates.get(pClientIP + pClientPort);
+
+        if (pMessage.equals("LOGOUT")) {
+            this.closeConnection(pClientIP, pClientPort);
+        } else if (pMessage.startsWith("USER")) {
+            if (state.loggedIn) {
+                this.send(pClientIP, pClientPort, "-ERR Already Logged in");
+            } else {
+                String suggestedUsername = pMessage.split(" ", 2)[1];
+                state.userName = suggestedUsername.replaceAll("\\$", "");
+                this.send(pClientIP, pClientPort, "+OK Password required");
+            }
+        } else if (pMessage.startsWith("PASS")) {
+            if (state.loggedIn) {
+                this.send(pClientIP, pClientPort, "-ERR Already Logged in");
+            } else {
+                this.send(pClientIP, pClientPort, "+OK Logged in as " + state.userName);
+                state.loggedIn = true;
+            }
+
+        } else if (pMessage.equals("WHOAMI")) {
+            // Undokumentierte anfrage (funktioniert nicht auf allen servern)
+            if (!state.loggedIn) {
+                this.send(pClientIP, pClientPort, "NOBODY");
+            } else {
+                this.send(pClientIP, pClientPort, state.userName);
+            }
+        } else {
+            this.send(pClientIP, pClientPort, "-ERR Command unrecognized");
+        }
     }
 
     @Override
@@ -39,5 +68,5 @@ public class ChatServer extends Server{
         // hier muss aufgeräumt werden was alles in ProcessNewConnection erschaffen wurde
         this.connectionStates.remove(pClientIP + pClientPort);
     }
-    
+
 }

b2c5f9b0329e75e7f025ce8bc21d87693f6a2cfc
JanzenJohn <work@johnjanzen.me>
2022-03-04 17:27:04 +0100

    Basic server der exit erlaubt

--- src/main/java/me/company/chatprojekt/ChatServer.java
+++ src/main/java/me/company/chatprojekt/ChatServer.java
@@ -4,10 +4,40 @@
  */
 package me.company.chatprojekt;
 
+import java.util.HashMap;
+
 /**
  *
  * @author john
  */
-public class ChatServer{
+public class ChatServer extends Server{
+    
+    public static void main(String[] args){
+        ChatServer x = new ChatServer();
+    }
+    private HashMap<String, ConnectionState> connectionStates;
+    
+    public ChatServer(){
+        super(4002);
+        this.connectionStates = new HashMap<>();
+    }
+
+    @Override
+    public void processNewConnection(String pClientIP, int pClientPort) {
+        this.connectionStates.put(pClientIP + pClientPort, new ConnectionState());
+        this.send(pClientIP, pClientPort, "+OK Server ready");
+    }
+
+    @Override
+    public void processMessage(String pClientIP, int pClientPort, String pMessage) {
+        if (pMessage.equals("EXIT"))this.closeConnection(pClientIP, pClientPort);
+        System.out.println(pClientIP + ":" +  pClientPort);
+    }
+
+    @Override
+    public void processClosingConnection(String pClientIP, int pClientPort) {
+        // hier muss aufgeräumt werden was alles in ProcessNewConnection erschaffen wurde
+        this.connectionStates.remove(pClientIP + pClientPort);
+    }
     
 }
---
+++ src/main/java/me/company/chatprojekt/ConnectionState.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package me.company.chatprojekt;
+
+/**
+ * Daten Klasse die alle eigenschaften des Verbindungspartners hält
+ * @author john
+ */
+public class ConnectionState {
+    boolean loggedIn = false;
+    String userName = null;
+}
